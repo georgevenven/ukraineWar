1106dbecdba0bcf391789cc02af0b9b16100d2be
georgeven <georgevenven@gmail.com>
2022-02-24 14:43:10 -0800

    proto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([49, 31], 6);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,5 +6,5 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZ2lib25mcm9nIiwiYSI6ImNsMDFqZ3Z3bzByczkza28wcHJ5Y3NibDkifQ.A3vza_R4MzWIztEj1LOCSg'
+    accessToken: 'pk.eyJ1IjoiZ2lib25mcm9nIiwiYSI6ImNsMDFqc2syczB1bGgza2xzcDIyMDh1cmcifQ.k8aEPsqM-ip0458DEvwIig'
 }).addTo(map);
