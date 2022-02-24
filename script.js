1b415c202a7f44ca4627ccad3a0735672fede038
georgeven <georgevenven@gmail.com>
2022-02-24 14:18:11 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ2lib25mcm9nIiwiYSI6ImNsMDFqZ3Z3bzByczkza28wcHJ5Y3NibDkifQ.A3vza_R4MzWIztEj1LOCSg'
+}).addTo(map);
